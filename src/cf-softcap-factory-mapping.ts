8d9a26344612d6443e8a0f136b1e4a16bb668d59
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-11-24 16:14:27 +0100

    add event handlers for cf-manager

--- src/cf-softcap-factory-mapping.ts
+++ src/cf-softcap-factory-mapping.ts
@@ -4,8 +4,11 @@ import {IAssetCommon} from '../generated/templates/CfManagerSoftcap/IAssetCommon
 import {DataSourceContext} from '@graphprotocol/graph-ts'
 
 export function handleCfManagerSoftcapCreated(event: CfManagerSoftcapCreated): void {
-  const issuerAddress = IAssetCommon.bind(event.params.asset).commonState().issuer
+  const assetCommonState = IAssetCommon.bind(event.params.asset).commonState()
+
   let context = new DataSourceContext()
-  context.setString('issuerAddress', issuerAddress.toHexString())
+  context.setString('issuerAddress', assetCommonState.issuer.toHexString())
+  context.setString('tokenSymbol', assetCommonState.symbol)
+
   CfManagerSoftcap.createWithContext(event.params.cfManager, context)
 }
--- src/cf-softcap-mapping.ts
+++ src/cf-softcap-mapping.ts
@@ -1,13 +1,69 @@
 import {CancelInvestment, Claim, Invest} from '../generated/templates/CfManagerSoftcap/CfManagerSoftcap'
+import {Transaction} from '../generated/schema'
+import {Address, BigInt, dataSource} from '@graphprotocol/graph-ts'
 
 export function handleInvest(event: Invest): void {
-  // TODO
+  const eventId = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
+  let transaction = new Transaction(eventId)
+  let context = dataSource.context()
+
+  transaction.fromAddress = event.params.investor
+  transaction.toAddress = event.address // TODO is this correct?
+  transaction.contract = event.address // TODO correct?
+  transaction.issuer = Address.fromString(context.getString('issuerAddress'))
+  transaction.hash = event.transaction.hash
+  transaction.type = 'ReserveInvestment'
+  transaction.asset = event.params.asset
+  transaction.timestamp = event.params.timestamp
+  transaction.tokenSymbol = context.getString('tokenSymbol')
+  transaction.tokenValue = event.params.tokenValue
+  transaction.tokenAmount = event.params.tokenAmount
+  transaction.payoutId = BigInt.zero() // TODO
+  transaction.revenue = BigInt.zero() // TODO
+
+  transaction.save()
 }
 
 export function handleClaim(event: Claim): void {
-  // TODO
+  const eventId = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
+  let transaction = new Transaction(eventId)
+  let context = dataSource.context()
+
+  transaction.fromAddress = event.address // TODO is this correct?
+  transaction.toAddress = event.params.investor // TODO correct?
+  transaction.contract = event.address // TODO correct?
+  transaction.issuer = Address.fromString(context.getString('issuerAddress'))
+  transaction.hash = event.transaction.hash
+  transaction.type = 'CompletedInvestment' // TODO correct?
+  transaction.asset = event.params.asset
+  transaction.timestamp = event.params.timestamp
+  transaction.tokenSymbol = context.getString('tokenSymbol')
+  transaction.tokenValue = event.params.tokenValue
+  transaction.tokenAmount = event.params.tokenAmount
+  transaction.payoutId = BigInt.zero() // TODO
+  transaction.revenue = BigInt.zero() // TODO
+
+  transaction.save()
 }
 
 export function handleCancelInvestment(event: CancelInvestment): void {
-  // TODO
+  const eventId = event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
+  let transaction = new Transaction(eventId)
+  let context = dataSource.context()
+
+  transaction.fromAddress = event.address // TODO is this correct?
+  transaction.toAddress = event.params.investor // TODO correct?
+  transaction.contract = event.address // TODO correct?
+  transaction.issuer = Address.fromString(context.getString('issuerAddress'))
+  transaction.hash = event.transaction.hash
+  transaction.type = 'CancelInvestment'
+  transaction.asset = event.params.asset
+  transaction.timestamp = event.params.timestamp
+  transaction.tokenSymbol = context.getString('tokenSymbol')
+  transaction.tokenValue = event.params.tokenValue
+  transaction.tokenAmount = event.params.tokenAmount
+  transaction.payoutId = BigInt.zero() // TODO
+  transaction.revenue = BigInt.zero() // TODO
+
+  transaction.save()
 }
